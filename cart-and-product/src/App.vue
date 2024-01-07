5386dbecb5c84c038635ce2146989b23e27b0b75
parsa.javadian <parsa.javadian@yahoo.com>
2024-01-07 20:24:14 +0330

    first commit

---
+++ cart-and-product/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+
+      <header class="top-bar spread">
+        <nav class="top-bar-nav">
+          <RouterLink to="/" class="top-bar-link">
+            <i class="icofont-spoon-and-fork"></i>
+            <span>Home</span>
+          </RouterLink>
+          <RouterLink to="/products" class="top-bar-link">
+            <span>Products</span>
+          </RouterLink>
+          <RouterLink to="/past-orders" class="top-bar-link">
+            <span>Past Orders</span>
+          </RouterLink>
+        </nav>
+        <!-- <RouterLink @click="toggleSidebar()" class="top-bar-cart-link">
+          <i class="icofont-cart-alt icofont-1x"></i>
+          <span>Cart ({{totalQuantity}})</span>
+        </RouterLink> -->
+      </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ cart-and-product/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template class="home">
+  <main>
+    Home
+  </main>
+</template>
